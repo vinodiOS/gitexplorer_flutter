0828ae726a299c785baa93fd6db84f6aebb8e431
Vinod Jagtap <vinod.jagtap@hotmail.com>
2022-09-02 15:00:56 +0530

    new loading gif

--- lib/screens/dashboard/components/result.dart
+++ lib/screens/dashboard/components/result.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Result extends StatelessWidget {
   final String commandText;
@@ -33,6 +36,7 @@ class Result extends StatelessWidget {
           data: commandText,
           isModeChanged: isModeChanged,
           isHighSpeed: speedChanged,
+          isData: true,
         ),
         const SizedBox(
           height: 30,
@@ -51,32 +55,61 @@ class Result extends StatelessWidget {
             data: noteText!,
             isModeChanged: isModeChanged,
             isHighSpeed: speedChanged,
+            isData: false,
           )
       ],
     );
   }
 }
 
-class ResultContainer extends StatelessWidget {
+class ResultContainer extends StatefulWidget {
   final Color backgroundColor;
   final String data;
   final bool isModeChanged;
   final bool isHighSpeed;
+  final bool isData;
   const ResultContainer(
       {super.key,
       required this.backgroundColor,
       required this.data,
       required this.isModeChanged,
-      required this.isHighSpeed});
+      required this.isHighSpeed,
+      required this.isData});
+  @override
+  State<StatefulWidget> createState() {
+    return ResultContainerState();
+  }
+}
+
+class ResultContainerState extends State<ResultContainer>
+    with TickerProviderStateMixin {
+  late Animation _heartAnimation;
+  late AnimationController _heartAnimationController;
+
+  @override
+  void initState() {
+    _heartAnimationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 800));
+    _heartAnimation = Tween(begin: 30.0, end: 40.0).animate(CurvedAnimation(
+        curve: Curves.bounceOut, parent: _heartAnimationController));
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _heartAnimationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       key: UniqueKey(),
       decoration: BoxDecoration(
-        color: backgroundColor,
+        color: widget.backgroundColor,
         borderRadius: BorderRadius.circular(10),
-        //border: Border.all(color: Colors.grey.shade300)
       ),
       child: IntrinsicHeight(
         child: Row(
@@ -95,7 +128,7 @@ class ResultContainer extends StatelessWidget {
             Expanded(
               child: Container(
                 constraints: const BoxConstraints(
-                  minHeight: 80,
+                  minHeight: 60,
                 ),
                 alignment: Alignment.centerLeft,
                 padding: const EdgeInsets.all(16),
@@ -105,14 +138,14 @@ class ResultContainer extends StatelessWidget {
                       fontSize: 16.0,
                       color: Colors.white,
                       fontWeight: FontWeight.w500),
-                  child: isModeChanged
-                      ? Text(data)
+                  child: widget.isModeChanged
+                      ? Text(widget.data)
                       : AnimatedTextKit(
                           isRepeatingAnimation: false,
                           key: UniqueKey(),
                           animatedTexts: [
-                            TyperAnimatedText(data,
-                                speed: isHighSpeed
+                            TyperAnimatedText(widget.data,
+                                speed: widget.isHighSpeed
                                     ? const Duration(milliseconds: 20)
                                     : const Duration(milliseconds: 40)),
                           ],
@@ -120,7 +153,31 @@ class ResultContainer extends StatelessWidget {
                         ),
                 ),
               ),
-            )
+            ),
+            if (widget.isData && widget.data.isNotEmpty)
+              AnimatedBuilder(
+                animation: _heartAnimationController,
+                builder: (context, child) {
+                  return Container(
+                    height: 60,
+                    padding: const EdgeInsets.only(right: 20),
+                    child: InkWell(
+                      onTap: () {
+                        Clipboard.setData(ClipboardData(text: widget.data));
+                        _heartAnimationController.forward();
+                        Timer(const Duration(milliseconds: 800), () {
+                          _heartAnimationController.reset();
+                        });
+                      },
+                      child: Icon(
+                        Icons.content_copy,
+                        color: Colors.white,
+                        size: _heartAnimation.value,
+                      ),
+                    ),
+                  );
+                },
+              ),
           ],
         ),
       ),
